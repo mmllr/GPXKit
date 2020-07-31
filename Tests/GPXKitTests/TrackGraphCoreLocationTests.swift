12c69fc4cd282a916b43a34e9593f24a7d329e58
Markus Mueller <mmlr@gmx.net>
2020-07-31 23:41:02 +0200

    Refactor tests for CoreLocation to separate test file
    Signed-off-by: Markus Mueller <mmlr@gmx.net>

---
+++ Tests/GPXKitTests/TrackGraphCoreLocationTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+@testable import GPXKit
+import CoreLocation
+
+extension TrackGraphTests {
+    // MARK: Tests
+
+    func testCLCoordinates2D() throws {
+        guard #available(macOS 10.12, iOS 8, *) else {
+            throw XCTSkip("Required API is not available for this test.")
+        }
+        let coordinates: [CLLocationCoordinate2D] = [
+                                                     CLLocationCoordinate2D(latitude: 51.2763320, longitude: 12.3767670),
+                                                     CLLocationCoordinate2D(latitude: 51.2763700, longitude: 12.3767550),
+                                                     CLLocationCoordinate2D(latitude: 51.2764100, longitude: 12.3767400),
+                                                     CLLocationCoordinate2D(latitude: 51.2764520, longitude: 12.3767260),
+                                                     CLLocationCoordinate2D(latitude: 51.2765020, longitude: 12.3767050),
+                                                     ]
+
+        XCTAssertEqual(coordinates, sut.coreLocationCoordinates)
+    }
+}
+
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        if lhs.latitude != rhs.latitude {
+            return false
+        }
+            return lhs.longitude == rhs.longitude
+            }
+}
+
--- Tests/GPXKitTests/TrackGraphTests.swift
+++ Tests/GPXKitTests/TrackGraphTests.swift
@@ -79,18 +79,6 @@ class TrackGraphTests: XCTestCase {
         XCTAssertEqual(373, sut.elevationGain)
     }
 
-    func testCLCoordinates2D() {
-        let coordinates: [CLLocationCoordinate2D] = [
-            CLLocationCoordinate2D(latitude: 51.2763320, longitude: 12.3767670),
-            CLLocationCoordinate2D(latitude: 51.2763700, longitude: 12.3767550),
-            CLLocationCoordinate2D(latitude: 51.2764100, longitude: 12.3767400),
-            CLLocationCoordinate2D(latitude: 51.2764520, longitude: 12.3767260),
-            CLLocationCoordinate2D(latitude: 51.2765020, longitude: 12.3767050),
-        ]
-
-        XCTAssertEqual(coordinates, sut.coreLocationCoordinates)
-    }
-
     func testInitializationFromGPX() {
         let points: [TrackPoint] = [
             givenAPoint(latitude: 51.2763320, longitude: 12.3767670, elevation: 1),
@@ -113,11 +101,4 @@ class TrackGraphTests: XCTestCase {
     }
 }
 
-extension CLLocationCoordinate2D: Equatable {
-    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
-        if lhs.latitude != rhs.latitude {
-            return false
-        }
-        return lhs.longitude == rhs.longitude
-    }
-}
+
