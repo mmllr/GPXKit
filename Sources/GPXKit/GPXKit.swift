d702423b35e669685299ae1ba94bd6d69eba4d41
Markus Mueller <mmlr@gmx.net>
2020-08-11 10:10:32 +0200

    Changes properties from let to var
    Signed-off-by: Markus Mueller <mmlr@gmx.net>

--- Sources/GPXKit/GPXKit.swift
+++ Sources/GPXKit/GPXKit.swift
@@ -1,36 +1,30 @@
 import Foundation
 
 public struct Coordinate: Equatable, Hashable {
-    public let latitude: Double
-    public let longitude: Double
-    public let elevation: Double
+    public var latitude: Double
+    public var longitude: Double
+    public var elevation: Double
 }
 
 public struct TrackSegment: Equatable {
-    public let coordinate: Coordinate
-    public let distanceInMeters: Double
+    public var coordinate: Coordinate
+    public var distanceInMeters: Double
 }
 
 public struct TrackPoint: Equatable, Hashable {
-    public let coordinate: Coordinate
-    public let date: Date?
-    public let power: Measurement<UnitPower>?
-
-    public init(coordinate: Coordinate, date: Date?, power: Measurement<UnitPower>? = nil) {
-        self.coordinate = coordinate
-        self.date = date
-        self.power = power
-    }
+    public var coordinate: Coordinate
+    public var date: Date?
+    public var power: Measurement<UnitPower>?
 }
 
 public struct TrackGraph: Equatable {
-    public let segments: [TrackSegment]
-    public let distance: Double
-    public let elevationGain: Double
+    public var segments: [TrackSegment]
+    public var distance: Double
+    public var elevationGain: Double
 }
 
 public struct GPXTrack: Equatable {
-    public let date: Date?
-    public let title: String
-    public let trackPoints: [TrackPoint]
+    public var date: Date?
+    public var title: String
+    public var trackPoints: [TrackPoint]
 }
