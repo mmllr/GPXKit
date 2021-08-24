dc02cefb55b245f1312d01a83bdef8dd63297de9
Markus Mueller <mmlr@gmx.net>
2021-08-24 18:44:27 +0200

    Adds initializers for Climb

--- Sources/GPXKit/GPXKit.swift
+++ Sources/GPXKit/GPXKit.swift
@@ -200,6 +200,47 @@ public struct Climb: Hashable {
     /// The second term in the formula is only added when it is positive, thay is, for climbs whose top is above 1000m.
     /// **NOTE** In GPXKit, the "(T - 1000)/1000" term of the FIETS formula is not added to the climb segments, so climbs can be joined together.
     public var score: Double
+
+    /// Intializes a `Climb`.
+    /// - Parameters:
+    ///   - start: The distance in meters from the `GPXTrack`s start.
+    ///   - end: The distance in meters from the `GOXTracks`s end.
+    ///   - bottom: The elevation in meters at the start of the climb.
+    ///   - top: The elevation in meters at the top of the climb.
+    ///   - totalElevation: The total elevation in meters of the climb (top - bottom in most cases).
+    ///   - grade: The grade (elevation over distance) of the climb in percent in the range {0,1}.
+    ///   - maxGrade: The maximum grade (elevation over distance) of the climb in percent in the range {0,1}.
+    ///   - score: The FIETS Score of the climb.
+    public init(start: Double, end: Double, bottom: Double, top: Double, totalElevation: Double, grade: Double, maxGrade: Double, score: Double) {
+        self.start = start
+        self.end = end
+        self.bottom = bottom
+        self.top = top
+        self.totalElevation = totalElevation
+        self.grade = grade
+        self.maxGrade = maxGrade
+        self.score = score
+    }
+
+    // Intializes a `Climb`.
+    /// - Parameters:
+    ///   - start: The distance in meters from the `GPXTrack`s start.
+    ///   - end: The distance in meters from the `GOXTracks`s end.
+    ///   - bottom: The elevation in meters at the start of the climb.
+    ///   - top: The elevation in meters at the top of the climb.
+    public init(start: Double, end: Double, bottom: Double, top: Double) {
+        let distance = end - start
+        let elevation = top - bottom
+        self.start = start
+        self.end = end
+        self.bottom = bottom
+        self.top = top
+        self.totalElevation = elevation
+        self.grade = elevation / distance
+        self.maxGrade = elevation / distance
+        self.score = (elevation * elevation) / (distance * 10) + max(0, (top - 1000.0) / 1000.0)
+    }
+
 }
 
 public extension Climb {
